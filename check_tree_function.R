d42e3ba3b15cb34b4a7dcd9e4075a658972fa499
flopidopi23 <fmluque@uc.cl>
2024-11-15 12:15:11 +0100

    checking the tree with the play_tenis data

---
+++ check_tree_function.R
@@ -0,0 +1,15 @@
+play_tennis <- read.csv('play_tennis.csv', stringsAsFactors = TRUE)
+# Save as .rda file in the 'data' directory of your project 
+save(play_tennis, file = "data/play_tennis.rda") 
+
+library(rpart)
+library(rpart.plot)
+model <- rpart(PlayTennis ~ ., play_tennis, control = rpart.control(cp = 0, maxdepth = 6, minsplit = 1, minbucket = 1)) 
+rpart.plot(model) 
+#when you use a little value for the maxdepth you make the tree more simplier. And increasing the value
+#make the tree more complex.
+# The complexity of the tree doesn't change when you put maxdepth <= 5 for this data 
+# This is because of the distributions of the nodes. In the final nodes for the more complex 
+# trees you have a perfect division. And in general this means that you probably will have a overfitting situation
+# with new data because this tree makes a perfect distribution of results.
+
